c43d855a1295042dc61906d688b271bdb83b1676
Milan <76396495+themilan1337@users.noreply.github.com>
2025-09-01 01:02:35 +0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -146,7 +146,7 @@
                     </template>
                 </ContainerScroll>
             </div>
-            <section class="bg-white dark:bg-zinc-900 rounded-3xl">
+            <section class="bg-white dark:bg-zinc-950 rounded-3xl">
                 <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
                     <div class="mx-auto max-w-screen-sm text-center">
                         <h2 class="mb-4 text-4xl tracking-tight font-extrabold leading-tight text-gray-900 dark:text-white">Start your free trial today</h2>
